0665a66ec40db1b21c232d3b745b54e38e5102a6
taehun0933 <taehun0933@naver.com>
2023-04-27 01:21:44 +0900

    기초 세팅1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <section>
+        <div className="text-center">Project</div>
+      </section>
+    </>
+  );
+}
