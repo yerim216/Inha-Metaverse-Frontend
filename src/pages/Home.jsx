252c1f5471e5848091bf8ec2e9be71e4e0474124
taehun0933 <taehun0933@naver.com>
2023-04-27 02:08:09 +0900

    아무말

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Dot from "../components/Dot";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <section>
-        <div className="text-center">Project</div>
+        <div className={styles.projectTitle}>
+          <Dot />
+          <h3>Map</h3>
+        </div>
+        <div className={styles.navBar}>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div>모집중</div>
+          </div>
+        </div>
       </section>
     </>
   );
