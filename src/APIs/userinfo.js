b19de7bd21a43fc1c05fae0cd76b5f3542c653e4
Jimin0430 <tangbole0430@gmail.com>
2023-12-20 01:30:30 +0900

    fix:invalid time value error fix

--- src/APIs/userinfo.js
+++ src/APIs/userinfo.js
@@ -1,127 +1,120 @@
-import axios from "axios";
+import axios from 'axios';
 
 // axios.defaults.baseURL = "https://app.vpspace.net/";
-axios.defaults.baseURL = "http://43.201.166.82:3000/";
+axios.defaults.baseURL = 'http://43.201.166.82:3000/';
 
 export function getUserIndex(userEmail, pw) {
-  return axios.post("/account", {
-    email: userEmail,
-    pw: pw,
-  });
+   return axios.post('/account', {
+      email: userEmail,
+      pw: pw,
+   });
 }
 
 export function getUserInfo(userIndex) {
-  return axios.post("/userinfo", {
-    index: userIndex,
-  });
+   return axios.post('/userinfo', {
+      index: userIndex,
+   });
 }
 
 export function getUserInterested() {
-  return axios.get("/userinfo/interested");
+   return axios.get('/userinfo/interested');
 }
 
 export function addInterested(userIndex, fieldIndex) {
-  return axios.post("/userinfo/interested/put", {
-    user_index: userIndex,
-    field_index: fieldIndex,
-  });
+   return axios.post('/userinfo/interested/put', {
+      user_index: userIndex,
+      field_index: fieldIndex,
+   });
 }
 
 export function getTeamIndex(userIndex) {
-  return axios.post("/userinfo/team", {
-    index: userIndex,
-  });
+   return axios.post('/userinfo/team', {
+      index: userIndex,
+   });
 }
 
 export function getSkills() {
-  return axios.get("/userinfo/skill");
+   return axios.get('/userinfo/skill');
 }
 
-export function putUserCareer(
-  userIndex,
-  userCareer,
-  startYear,
-  startMonth,
-  endYear,
-  endMonth
-) {
-  // console.log({
-  //   index: userIndex,
-  //   career: userCareer,
-  //   start_year: startYear,
-  //   start_month: startMonth,
-  //   end_year: endYear,
-  //   end_month: endMonth,
-  // });
+export function putUserCareer(userIndex, userCareer, startYear, startMonth, endYear, endMonth) {
+   console.log({
+      index: userIndex,
+      career: userCareer,
+      start_year: startYear,
+      start_month: startMonth,
+      end_year: endYear,
+      end_month: endMonth,
+   });
 
-  return axios.post("/userinfo/put/career", {
-    index: userIndex,
-    career: userCareer,
-    start_year: startYear,
-    start_month: startMonth,
-    end_year: endYear,
-    end_month: endMonth,
-  });
+   return axios.post('/userinfo/put/career', {
+      index: userIndex,
+      career: userCareer,
+      start_year: startYear,
+      start_month: startMonth,
+      end_year: endYear,
+      end_month: endMonth,
+   });
 }
 
-export function getUserCareer(userIndex, userCareer) {
-  return axios.post("/userinfo/get/career", {
-    user: userIndex,
-  });
+export function getUserCareer(userIndex) {
+   return axios.post('/userinfo/get/career', {
+      user: userIndex,
+   });
 }
 
 export function putUserIntroduction(userIndex, userIntroduction) {
-  return axios.post("/userinfo/put/introduction", {
-    index: userIndex,
-    introduction: userIntroduction,
-  });
+   return axios.post('/userinfo/put/introduction', {
+      index: userIndex,
+      introduction: userIntroduction,
+   });
 }
 
 export function putUserJob(userIndex, userJob) {
-  return axios.post("/userinfo/put/job", {
-    index: userIndex,
-    job: userJob,
-  });
+   return axios.post('/userinfo/put/job', {
+      index: userIndex,
+      job: userJob,
+   });
 }
 
 export function putUserInterest(userIndex, userInterest) {
-  return axios.post("/userinfo/interested/put", {
-    user_index: userIndex,
-    field_index: userInterest,
-  });
+   return axios.post('/userinfo/interested/put', {
+      user_index: userIndex,
+      field_index: userInterest,
+   });
 }
 
 export function deleteUserInterest(userIndex, userInterest) {
-  return axios.post("/userinfo/interested/delete", {
-    user_index: userIndex,
-    field_index: userInterest,
-  });
+   return axios.post('/userinfo/interested/delete', {
+      user_index: userIndex,
+      field_index: userInterest,
+   });
 }
 
 export function putUserSkill(userIndex, skillIndex) {
-  return axios.post("/userinfo/skill/put", {
-    user_index: userIndex,
-    skill_index: skillIndex,
-  });
+   return axios.post('/userinfo/skill/put', {
+      user_index: userIndex,
+      skill_index: skillIndex,
+   });
 }
 
 export function deleteUserSkill(userIndex, skillIndex) {
-  return axios.post("/userinfo/skill/delete", {
-    user_index: userIndex,
-    skill_index: skillIndex,
-  });
+   return axios.post('/userinfo/skill/delete', {
+      user_index: userIndex,
+      skill_index: skillIndex,
+   });
 }
 
 export function putUserProfileImg(userIndex, imgIndex) {
-  return axios.post("/userinfo/put/img", {
-    index: userIndex,
-    image: imgIndex,
-  });
+   return axios.post('/userinfo/put/img', {
+      index: userIndex,
+      image: imgIndex,
+   });
 }
 
 export function putUserImg(userIndex, userImg) {
-  return axios.post("/userinfo/put/img", {
-    index: userIndex,
-    image: userImg,
-  });
+   return axios.post('/userinfo/put/img', {
+      index: userIndex,
+      image: userImg,
+   });
 }
--- src/components/SetEventModal.jsx
+++ src/components/SetEventModal.jsx
@@ -1,319 +1,317 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useOutletContext, useHistory } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { userState } from "../recoil";
-import Modal from "react-modal";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"; // DatePicker 스타일 가져오기
-import "../styles/SetEventModal.css";
-import { addScheduleByToDo, deleteEvent, modifyEvent } from "../APIs/schedule";
+import React, { useState, useRef, useEffect } from 'react';
+import { useOutletContext, useHistory } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { userState } from '../recoil';
+import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // DatePicker 스타일 가져오기
+import '../styles/SetEventModal.css';
+import { addScheduleByToDo, deleteEvent, modifyEvent } from '../APIs/schedule';
 
 function ModalComponent({
-  isOpen,
-  onRequestClose,
-  startDay,
-  endDay,
-  title,
-  scheduleIndex,
-  eventColor,
-  note,
-  today,
-  setEventChange,
+   isOpen,
+   onRequestClose,
+   startDay,
+   endDay,
+   title,
+   scheduleIndex,
+   eventColor,
+   note,
+   today,
+   setEventChange,
+   setUpdate,
 }) {
-  const { teamIndex } = useOutletContext();
+   const { teamIndex } = useOutletContext();
 
-  // console.log(today);
-  const [userLogin, setUserLogin] = useRecoilState(userState);
-  const userIndex = userLogin.user_index;
+   // console.log(today);
+   const [userLogin, setUserLogin] = useRecoilState(userState);
+   const userIndex = userLogin.user_index;
 
-  const [inputValue, setInputValue] = useState(""); //event 제목 입력 관리
-  const inputRef = useRef(null);
+   const [inputValue, setInputValue] = useState(''); //event 제목 입력 관리
+   const inputRef = useRef(null);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+   const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+   };
 
-  const [inputNoteValue, setInputNoteValue] = useState(""); //event 메모 입력 관리
-  const inputNoteRef = useRef(null);
+   const [inputNoteValue, setInputNoteValue] = useState(''); //event 메모 입력 관리
+   const inputNoteRef = useRef(null);
 
-  const handleInputNoteChange = (event) => {
-    setInputNoteValue(event.target.value);
-  };
+   const handleInputNoteChange = (event) => {
+      setInputNoteValue(event.target.value);
+   };
 
-  const [selectedStartDate, setSelectedStartDate] = useState(new Date()); //event 시작 날짜 입력 관리
-  const [selectedEndDate, setSelectedEndDate] = useState(new Date()); //event 종료 날짜 입력 관리
+   const [selectedStartDate, setSelectedStartDate] = useState(new Date()); //event 시작 날짜 입력 관리
+   const [selectedEndDate, setSelectedEndDate] = useState(new Date()); //event 종료 날짜 입력 관리
 
-  const handleStartDateChange = (date) => {
-    setSelectedStartDate(date);
-  };
+   const handleStartDateChange = (date) => {
+      setSelectedStartDate(date);
+   };
 
-  const handleEndDateChange = (date) => {
-    setSelectedEndDate(date);
-  };
+   const handleEndDateChange = (date) => {
+      setSelectedEndDate(date);
+   };
 
-  const [selectedColor, setSelectedColor] = useState(null);
-  const colors = [
-    { name: "기획", code: "#FBEFB5" },
-    { name: "개발", code: "#C1E8E4" },
-    { name: "디자인", code: "#FBEAE9" },
-    { name: "그 외", code: "#E9F0FE" },
-  ];
+   const [selectedColor, setSelectedColor] = useState(null);
+   const colors = [
+      { name: '기획', code: '#FBEFB5' },
+      { name: '개발', code: '#C1E8E4' },
+      { name: '디자인', code: '#FBEAE9' },
+      { name: '그 외', code: '#E9F0FE' },
+   ];
 
-  useEffect(() => {
-    setSelectedStartDate(today);
-    setSelectedEndDate(today);
-  }, [today]);
+   useEffect(() => {
+      today && setSelectedStartDate(today);
+      today && setSelectedEndDate(today);
+   }, [today]);
 
-  useEffect(() => {
-    if (startDay !== null) {
-      setSelectedStartDate(startDay);
-    }
-    if (endDay !== null) {
-      setSelectedEndDate(endDay);
-    }
-    if (eventColor === null && selectedColor === null) {
-      eventColor = "#E9F0FE";
-    }
-    if (eventColor != null) {
-      setSelectedColor(eventColor);
-    }
-    if (title !== null) {
-      setInputValue(title);
-    }
-    if (note !== null) {
-      setInputNoteValue(note);
-    }
-  }, [startDay]);
+   useEffect(() => {
+      if (startDay !== null) {
+         setSelectedStartDate(startDay);
+      }
+      if (endDay !== null) {
+         setSelectedEndDate(endDay);
+      }
+      if (eventColor === null && selectedColor === null) {
+         eventColor = '#E9F0FE';
+      }
+      if (eventColor != null) {
+         setSelectedColor(eventColor);
+      }
+      if (title !== null) {
+         setInputValue(title);
+      }
+      if (note !== null) {
+         setInputNoteValue(note);
+      }
+   }, [startDay]);
 
-  //이벤트 삭제
-  const handleDelete = (deleteEventIndex) => {
-    const number = parseInt(deleteEventIndex, 10); // 10진수로 변환
+   //이벤트 삭제
+   const handleDelete = (deleteEventIndex) => {
+      const number = parseInt(deleteEventIndex, 10); // 10진수로 변환
 
-    deleteEvent(number)
-      .then((response) => {
-        console.log("이벤트 삭제 완료 : " + response);
+      deleteEvent(number)
+         .then((response) => {
+            console.log('이벤트 삭제 완료 : ' + response);
+            setUpdate(true);
+         })
+         .catch((error) => {
+            console.error('Error delete event from DB:', error);
+         });
+
+      onRequestClose();
+      setInputValue('');
+      setInputNoteValue('');
+      setSelectedStartDate(new Date());
+      setSelectedEndDate(new Date());
+   };
+
+   const startDateObject = new Date(selectedStartDate);
+   const sYear = startDateObject.getFullYear();
+   const sMonth = String(startDateObject.getMonth() + 1).padStart(2, '0');
+   const sDay = String(startDateObject.getDate()).padStart(2, '0');
+   const formattedStartDate = `${sYear}${sMonth}${sDay}`;
+
+   const endDateObject = new Date(selectedEndDate);
+   const eYear = endDateObject.getFullYear();
+   const eMonth = String(endDateObject.getMonth() + 1).padStart(2, '0');
+   const eDay = String(endDateObject.getDate()).padStart(2, '0');
+   const formattedEndDate = `${eYear}${eMonth}${eDay}`;
+
+   const createTime = new Date();
+   const createTimestamp = createTime.toISOString();
+   const status = '1';
+
+   const addEventsToDB = async () => {
+      addScheduleByToDo({
+         team: parseInt(teamIndex),
+         title: inputValue || 'New Event',
+         content: inputNoteValue,
+         status: 1,
+         start_date: formattedStartDate, //20231126
+         end_date: formattedEndDate,
+         writer: userIndex,
+         created_at: createTimestamp,
+         color: selectedColor,
       })
-      .catch((error) => {
-        console.error("Error delete event from DB:", error);
-      });
+         .then(function () {
+            console.log('db에 일정 추가 성공');
+            //   console.log(formattedStartDate + " " + formattedEndDate);
+         })
+         .catch(function (error) {
+            console.log(error);
+         });
+   };
 
-    onRequestClose();
-    setInputValue("");
-    setInputNoteValue("");
-    setSelectedStartDate(new Date());
-    setSelectedEndDate(new Date());
-  };
-
-  const startDateObject = new Date(selectedStartDate);
-  const sYear = startDateObject.getFullYear();
-  const sMonth = String(startDateObject.getMonth() + 1).padStart(2, "0");
-  const sDay = String(startDateObject.getDate()).padStart(2, "0");
-  const formattedStartDate = `${sYear}${sMonth}${sDay}`;
-
-  const endDateObject = new Date(selectedEndDate);
-  const eYear = endDateObject.getFullYear();
-  const eMonth = String(endDateObject.getMonth() + 1).padStart(2, "0");
-  const eDay = String(endDateObject.getDate()).padStart(2, "0");
-  const formattedEndDate = `${eYear}${eMonth}${eDay}`;
-
-  const createTime = new Date();
-  const createTimestamp = createTime.toISOString();
-  const status = "1";
-
-  const addEventsToDB = async () => {
-    addScheduleByToDo({
-      team: parseInt(teamIndex),
-      title: inputValue || "New Event",
-      content: inputNoteValue,
-      status: 1,
-      start_date: formattedStartDate, //20231126
-      end_date: formattedEndDate,
-      writer: userIndex,
-      created_at: createTimestamp,
-      color: selectedColor,
-    })
-      .then(function () {
-        console.log("db에 일정 추가 성공");
-        //   console.log(formattedStartDate + " " + formattedEndDate);
+   const modifyEvents = () => {
+      modifyEvent({
+         team: parseInt(teamIndex),
+         title: inputValue || 'New Event',
+         content: inputNoteValue,
+         status: 1,
+         start_date: formattedStartDate,
+         end_date: formattedEndDate,
+         writer: userIndex,
+         last_update: createTimestamp,
+         color: selectedColor,
       })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+         .then(function () {
+            console.log('일정 수정 성공');
+         })
+         .catch(function (error) {
+            console.log('일정 수정 실패' + error);
+         });
+   };
 
-  const modifyEvents = () => {
-    modifyEvent({
-      team: parseInt(teamIndex),
-      title: inputValue || "New Event",
-      content: inputNoteValue,
-      status: 1,
-      start_date: formattedStartDate,
-      end_date: formattedEndDate,
-      writer: userIndex,
-      last_update: createTimestamp,
-      color: selectedColor,
-    })
-      .then(function () {
-        console.log("일정 수정 성공");
-      })
-      .catch(function (error) {
-        console.log("일정 수정 실패" + error);
-      });
-  };
+   const handleClose = () => {
+      // scheduleIndex === null ? addEventsToDB() : modifyEvents();
+      addEventsToDB();
+      setUpdate(true);
 
-  const handleClose = () => {
-    // scheduleIndex === null ? addEventsToDB() : modifyEvents();
-    addEventsToDB();
-    setEventChange(true);
-    onRequestClose();
-    setInputValue("");
-    setInputNoteValue("");
-    setSelectedStartDate(new Date());
-    setSelectedEndDate(new Date());
-    setSelectedColor(null);
-  };
+      setEventChange(true);
+      onRequestClose();
+      setInputValue('');
+      setInputNoteValue('');
+      setSelectedStartDate(new Date());
+      setSelectedEndDate(new Date());
+      setSelectedColor(null);
+   };
 
-  const handleClose2 = () => {
-    handleDelete(scheduleIndex);
-    addEventsToDB();
-    // modifyEvents();
-    setEventChange(true);
+   const handleClose2 = () => {
+      handleDelete(scheduleIndex);
+      addEventsToDB();
+      // modifyEvents();
+      setUpdate(true);
 
-    onRequestClose();
-    setInputValue("");
-    setInputNoteValue("");
-    setSelectedStartDate(new Date());
-    setSelectedEndDate(new Date());
-    setSelectedColor(null);
-  };
+      setEventChange(true);
 
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="이벤트 등록"
-      style={{
-        overlay: {
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: "rgba(105, 105, 105, 0.8)",
-          zIndex: "9999",
-        },
-        content: {
-          position: "absolute",
-          top: "5vh",
-          left: "10vw",
-          right: "5vw",
-          bottom: "5vh",
-          border: "none",
-          background: "rgb(50,50,50)",
-          overflow: "auto",
-          WebkitOverflowScrolling: "touch",
-          borderRadius: "50px",
-          outline: "none",
-          padding: "3% 5% 1% 5%",
-        },
-      }}
-    >
-      <div className="input-container">
-        <input
-          ref={inputRef}
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          className="custom-input"
-          placeholder={`${inputValue ? { inputValue } : "New Event"}`}
-        />
-      </div>
+      onRequestClose();
+      setInputValue('');
+      setInputNoteValue('');
+      setSelectedStartDate(new Date());
+      setSelectedEndDate(new Date());
+      setSelectedColor(null);
+      setUpdate(true);
+   };
 
-      <div className="dateContainer">
-        <p>Start Date</p>
-        <DatePicker
-          className="custom-startDateInput"
-          selected={selectedStartDate}
-          onChange={handleStartDateChange}
-          popperPlacement="bottom-start"
-          withPortal
-          showYearDropdown
-          dateFormatCalendar="MMMM yyyy"
-          dateFormat="MMMM d, yyyy"
-        />
-      </div>
+   return (
+      <Modal
+         isOpen={isOpen}
+         onRequestClose={onRequestClose}
+         contentLabel="이벤트 등록"
+         style={{
+            overlay: {
+               position: 'fixed',
+               top: 0,
+               left: 0,
+               right: 0,
+               bottom: 0,
+               backgroundColor: 'rgba(105, 105, 105, 0.8)',
+               zIndex: '9999',
+            },
+            content: {
+               position: 'absolute',
+               top: '5vh',
+               left: '10vw',
+               right: '5vw',
+               bottom: '5vh',
+               border: 'none',
+               background: 'rgb(50,50,50)',
+               overflow: 'auto',
+               WebkitOverflowScrolling: 'touch',
+               borderRadius: '50px',
+               outline: 'none',
+               padding: '3% 5% 1% 5%',
+            },
+         }}
+      >
+         <div className="input-container">
+            <input
+               ref={inputRef}
+               type="text"
+               value={inputValue}
+               onChange={handleInputChange}
+               className="custom-input"
+               placeholder={`${inputValue ? { inputValue } : 'New Event'}`}
+            />
+         </div>
 
-      <div className="dateContainer">
-        <p>End Date </p>
-        <DatePicker
-          className="custom-endDateInput"
-          selected={selectedEndDate}
-          onChange={handleEndDateChange}
-          popperPlacement="bottom-start"
-          withPortal
-          showYearDropdown
-          dateFormatCalendar="MMMM yyyy"
-          dateFormat="MMMM d, yyyy"
-        />
-      </div>
+         <div className="dateContainer">
+            <p>Start Date</p>
+            <DatePicker
+               className="custom-startDateInput"
+               selected={selectedStartDate}
+               onChange={handleStartDateChange}
+               popperPlacement="bottom-start"
+               withPortal
+               showYearDropdown
+               dateFormatCalendar="MMMM yyyy"
+               dateFormat="MMMM d, yyyy"
+            />
+         </div>
 
-      <div className="cate-select">
-        <p>Category </p>
+         <div className="dateContainer">
+            <p>End Date </p>
+            <DatePicker
+               className="custom-endDateInput"
+               selected={selectedEndDate}
+               onChange={handleEndDateChange}
+               popperPlacement="bottom-start"
+               withPortal
+               showYearDropdown
+               dateFormatCalendar="MMMM yyyy"
+               dateFormat="MMMM d, yyyy"
+            />
+         </div>
 
-        {colors.map((color, index) => {
-          return (
-            <div
-              key={index}
-              className="cate-inbox"
-              onClick={() => setSelectedColor(color.code)}
+         <div className="cate-select">
+            <p>Category </p>
+
+            {colors.map((color, index) => {
+               return (
+                  <div key={index} className="cate-inbox" onClick={() => setSelectedColor(color.code)}>
+                     <div
+                        className={`color-circle ${selectedColor === color.code ? 'selected' : ''}`}
+                        style={{ backgroundColor: color.code }}
+                     ></div>
+                     <span className="label">{color.name}</span>
+                  </div>
+               );
+            })}
+         </div>
+
+         <div className="line"></div>
+
+         <div className="input-container">
+            <textarea
+               ref={inputNoteRef}
+               value={inputNoteValue}
+               onChange={handleInputNoteChange}
+               className="custom-noteInput"
+               placeholder="Add Note"
+            />
+         </div>
+
+         <div className="buttonWrap">
+            {scheduleIndex === null ? (
+               <button className="closeButton" onClick={() => handleClose()}>
+                  Save
+               </button>
+            ) : (
+               <button className="closeButton" onClick={() => handleClose2(scheduleIndex)}>
+                  Save
+               </button>
+            )}
+            <button
+               className="deleteButton"
+               style={{ display: scheduleIndex === null ? 'none' : 'block' }}
+               onClick={() => handleDelete(scheduleIndex)}
             >
-              <div
-                className={`color-circle ${
-                  selectedColor === color.code ? "selected" : ""
-                }`}
-                style={{ backgroundColor: color.code }}
-              ></div>
-              <span className="label">{color.name}</span>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="line"></div>
-
-      <div className="input-container">
-        <textarea
-          ref={inputNoteRef}
-          value={inputNoteValue}
-          onChange={handleInputNoteChange}
-          className="custom-noteInput"
-          placeholder="Add Note"
-        />
-      </div>
-
-      <div className="buttonWrap">
-        {scheduleIndex === null ? (
-          <button className="closeButton" onClick={() => handleClose()}>
-            Save
-          </button>
-        ) : (
-          <button
-            className="closeButton"
-            onClick={() => handleClose2(scheduleIndex)}
-          >
-            Save
-          </button>
-        )}
-        <button
-          className="deleteButton"
-          style={{ display: scheduleIndex === null ? "none" : "block" }}
-          onClick={() => handleDelete(scheduleIndex)}
-        >
-          Delete
-        </button>
-      </div>
-    </Modal>
-  );
+               Delete
+            </button>
+         </div>
+      </Modal>
+   );
 }
 
 export default ModalComponent;
