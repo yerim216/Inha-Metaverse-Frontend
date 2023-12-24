539de783d93a32828fd2c59923725fc9b1683a6c
taehun0933 <taehun0933@naver.com>
2023-12-24 15:58:30 +0900

    test

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ if (localStorage.getItem("recoil-persist") === null) {
 let isMobile = navigator.userAgentData.mobile;
 
 root.render(
-  <ThemeProvider>{!isMobile ? <ForMobile /> : <App />}</ThemeProvider>
+  <ThemeProvider>{isMobile ? <ForMobile /> : <App />}</ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
