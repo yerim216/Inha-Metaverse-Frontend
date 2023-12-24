e2194c94a6265f7905a4ad87dbcb35ad0242c737
taehun0933 <taehun0933@naver.com>
2023-12-24 15:38:37 +0900

    Fix : 모바일 처리 변경

--- src/components/common/ForMobile.jsx
+++ src/components/common/ForMobile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ForMobile() {
   return (
     <section className="w-screen h-screen bg-white flex items-center justify-center">
-      <pre className="text-center font-bold animate-bounce text-base whitespace-pre-wrap">
+      <pre className="text-center font-bold animate-bounce text-2xl text-black">
         {
           "모바일 환경에 접속 중이신 것으로 보여요.\n모바일 환경은 준비 중이니 잠시만 기다려 주세요!"
         }
