ae0e88db4b1d4c46d645c9347473563006a52483
Jimin0430 <tangbole0430@gmail.com>
2023-12-27 16:14:43 +0900

    api url https로 변경

--- src/APIs/download.js
+++ src/APIs/download.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-// axios.defaults.baseURL = "https://app.vpspace.net/";
-axios.defaults.baseURL = " http://43.201.166.82:3000/";
+axios.defaults.baseURL = 'https://app.vpspace.net/';
+// axios.defaults.baseURL = " http://43.201.166.82:3000/";
 
 export function getDownload() {
-  return axios.get("/get/download/archifree/build.zip");
+   return axios.get('/get/download/archifree/build.zip');
 }
